c792d56606c954609ba902685b92c99e99bf28e9
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-01-24 18:59:32 +0100

    initialize repositorie

---
+++ index.js
@@ -0,0 +1,26 @@
+const main = document.querySelector('main');
+
+let exerciceArray = [
+  { pic: 0, min: 1 },
+  { pic: 1, min: 1 },
+  { pic: 2, min: 1 },
+  { pic: 3, min: 1 },
+  { pic: 4, min: 1 },
+  { pic: 5, min: 1 },
+  { pic: 6, min: 1 },
+  { pic: 7, min: 1 },
+  { pic: 8, min: 1 },
+  { pic: 9, min: 1 },
+]
+
+class Ewercice {}
+
+const utils = {
+
+}
+
+const page = {
+  lobby: function() {
+    
+  }
+}
