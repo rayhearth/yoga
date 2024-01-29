f6ef413b5fbc90ac23ceca241ac4a25167abf663
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-01-29 19:55:02 +0100

    create handleEventArrow function

--- index.js
+++ index.js
@@ -1,64 +1,98 @@
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 
 let exerciceArray = [
-  { pic: 0, min: 1 },
-  { pic: 1, min: 1 },
-  { pic: 2, min: 1 },
-  { pic: 3, min: 1 },
-  { pic: 4, min: 1 },
-  { pic: 5, min: 1 },
-  { pic: 6, min: 1 },
-  { pic: 7, min: 1 },
-  { pic: 8, min: 1 },
-  { pic: 9, min: 1 },
-]
+	{ pic: 0, min: 1 },
+	{ pic: 1, min: 1 },
+	{ pic: 2, min: 1 },
+	{ pic: 3, min: 1 },
+	{ pic: 4, min: 1 },
+	{ pic: 5, min: 1 },
+	{ pic: 6, min: 1 },
+	{ pic: 7, min: 1 },
+	{ pic: 8, min: 1 },
+	{ pic: 9, min: 1 },
+];
 
 class Exercice {}
 
 const utils = {
+	pageContent: function (title, content, btn) {
+		document.querySelector("h1").innerHTML = title;
+		main.innerHTML = content;
+		document.querySelector(".btn-container").innerHTML = btn;
+	},
 
-  pageContent : function(title, content, btn){
-    document.querySelector("h1").innerHTML = title;
-    main.innerHTML = content;
-    document.querySelector(".btn-container").innerHTML = btn;
-  }
-}
+	handleEventMinutes: function () {
+		document.querySelectorAll('input[type="number"]').forEach((input) => {
+			input.addEventListener("input", (e) => {
+				exerciceArray.map((exo) => {
+					if (exo.pic == e.target.id) {
+						exo.min = parseInt(e.target.value);
+					}
+				});
+			});
+		});
+	},
+
+	handleEventArrow: function () {
+		document.querySelectorAll(".arrow").forEach((arrow) => {
+			arrow.addEventListener("click", (e) => {
+				let position = 0;
+				exerciceArray.map((exo) => {
+					if (exo.pic == e.target.dataset.pic && position != 0) {
+						[exerciceArray[position], exerciceArray[position - 1]] = [
+							exerciceArray[position - 1],
+							exerciceArray[position],
+						];
+						page.lobby();
+					} else {
+						position++;
+					}
+				});
+			});
+		});
+	},
+};
 
 const page = {
-  lobby: function() {
-    let mapArray = exerciceArray
-    .map(
-      (exo) =>
-      `
+	lobby: function () {
+		let mapArray = exerciceArray
+			.map(
+				(exo) =>
+					`
       <li>
         <div class="card-header">
-          <input type="number" id)${exo.pic} min="1" max="10" value=${exo.min}>
-          <span>min</span>
+        <input type="number" id=${exo.pic} min="1" max="10" value=${exo.min}>
+        <span>min</span>
         </div>
+        <img src="./img/${exo.pic}.png" />
+        <i class="fas fa-arrow-alt-circle-left arrow" data-pic=${exo.pic}></i>
+        <i class="fas fa-times-circle deleteBtn" data-pic=${exo.pic}></i>
       </li>
-      `,
-    )
-    .join();
+      `
+			)
+			.join("");
 
-    utils.pageContent(
-      "Paramétrage <i id='reboot' class='fas fa-undo'></i>",
-      "<ul>" + mapArray + "</ul>",
-      "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>",
-      );
-    },
-    
-    routine: function() {
-      utils.pageContent(
-      "Routine","Exercice avec chrono", null);
-  },
-  
-  finish: function() {
-    utils.pageContent(
-    "C'est terminé",
-    "<button id='start'>Recommencer</button>",
-    "<button id='reboot' class='btn-reboot'>Réinitialiser<i class='far fa-play-circle'></i></button>",
-  );
-  },
+		utils.pageContent(
+			"Paramétrage <i id='reboot' class='fas fa-undo'></i>",
+			"<ul>" + mapArray + "</ul>",
+			"<button id='start'>Commencer<i class='far fa-play-circle'></i></button>"
+		);
+		utils.handleEventMinutes();
+		utils.handleEventArrow();
+	},
+
+	routine: function () {
+		utils.pageContent("Routine", "Exercice avec chrono", null);
+	},
+
+	finish: function () {
+		utils.pageContent(
+			"C'est terminé",
+			"<button id='start'>Recommencer</button>",
+			"<button id='reboot' class='btn-reboot'>Réinitialiser<i class='far fa-play-circle'></i></button>"
+		);
+	},
 };
 
 page.lobby();
