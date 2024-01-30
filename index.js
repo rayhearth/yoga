509f786b270414277f99056794d26ba0f23b1204
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-01-30 13:56:29 +0100

    add delete function and locxalstorage for remember prefrences

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const main = document.querySelector("main");
-
-let exerciceArray = [
+const basicArray = [
 	{ pic: 0, min: 1 },
 	{ pic: 1, min: 1 },
 	{ pic: 2, min: 1 },
@@ -12,6 +11,17 @@ let exerciceArray = [
 	{ pic: 8, min: 1 },
 	{ pic: 9, min: 1 },
 ];
+
+let exerciceArray = [];
+
+//Get stored exercices array
+(() => {
+	if (localStorage.exercices) {
+		exerciceArray = localStorage.exercices;
+	} else {
+		exerciceArray = basicArray;
+	}
+})();
 
 class Exercice {}
 
@@ -28,6 +38,7 @@ const utils = {
 				exerciceArray.map((exo) => {
 					if (exo.pic == e.target.id) {
 						exo.min = parseInt(e.target.value);
+						this.store()
 					}
 				});
 			});
@@ -45,12 +56,39 @@ const utils = {
 							exerciceArray[position],
 						];
 						page.lobby();
+						this.store()
 					} else {
 						position++;
 					}
 				});
 			});
 		});
+	},
+
+	deleteItem: function () {
+		document.querySelectorAll(".deleteBtn").forEach((btn) => {
+			btn.addEventListener("click", (e) => {
+				let newArr = [];
+				exerciceArray.map((exo) => {
+					if (exo.pic != e.target.dataset.pic) {
+						newArr.push(exo);
+					}
+				});
+				exerciceArray = newArr;
+				page.lobby();
+				this.store()
+			});
+		});
+	},
+
+	reboot: function () {
+		exerciceArray = basicArray;
+		page.lobby();
+		this.store();
+	},
+
+	store: function () {
+		localStorage.exercices = exerciceArray;
 	},
 };
 
@@ -80,6 +118,8 @@ const page = {
 		);
 		utils.handleEventMinutes();
 		utils.handleEventArrow();
+		utils.deleteItem();
+		reboot.addEventListener("click", () => utils.reboot());
 	},
 
 	routine: function () {
